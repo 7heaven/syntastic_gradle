daaa0459d7e1be61c99380e41c5c247d06b5b15b
7heaven <caifangmao8@gmail.com>
2018-12-01 20:50:04 +0800

    first commit

---
+++ syntastic_gradle_sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -n 'build.gradle' ]]
+then
+    echo "apply from: '$(dirname $0)/syntastic.gradle'" >> build.gradle
+
+    if [[ -z '$1' ]]
+    then
+        1=''
+    fi
+
+    ./gradlew clean
+    ./gradlew assembleRelease $1
+  
+    grep -v "syntastic\.gradle" build.gradle > build.gradle.tmp.syntastic && mv build.gradle.tmp.syntastic build.gradle
+fi
